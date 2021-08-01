bf583f28e6e2d934af991808a62148c5301ea83c
ericksonder <ericksonder017@gmail.com>
2021-08-01 03:09:33 -0500

    Agregue el mensaje cargando

--- src/App.js
+++ src/App.js
@@ -1,39 +1,40 @@
-import React, {useEffect, useState} from 'react'
-import Galery from './components/galery/Galery'
-import SearchImage from './components/searchImage/SearchImage'
-import {  getImages, getImagesBySearch } from './services/pexelsApi'
+import React, { useEffect, useState } from "react";
+import Galery from "./components/galery/Galery";
+import SearchImage from "./components/searchImage/SearchImage";
+import { getImages, getImagesBySearch } from "./services/pexelsApi";
 
 const App = () => {
-
+  const [cargando, setCargando] = useState(true);
 
   const [images, setImages] = useState([]);
 
-    useEffect(() => {
-        getImages().then(data => {
-          setImages(data)
-        });
-
-    }, [])
+  useEffect(() => {
+    getImages().then((data) => {
+      setImages(data);
+      setCargando(false);
+    });
+  }, []);
 
   const handleSearchImg = (word) => {
-    getImagesBySearch(word).then(data => {
-      setImages(data)
-    })
-  }
+    setCargando(true)
+    getImagesBySearch(word).then((data) => {
+      setImages(data);
+      setCargando(false)
+    });
+  };
 
-
- 
   return (
     <div>
       <SearchImage handleSearchImg={handleSearchImg} />
-      {
-        images.length > 0 ?
-        <Galery images={images}/>
-        :
-        <p style={{"textAlign": "center", "fontSize": "1.5em"}} >Sin resultados</p>
-      }
+      {cargando ? (
+        <p style={{ fontSize: "1.5em" }}>Cargando...</p>
+      ) : images.length > 0 ? (
+        <Galery images={images} />
+      ) : (
+        <p style={{ fontSize: "1.5em" }}>Sin resultados</p>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
